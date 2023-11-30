b5d539637064e471ae4d399cc82d532bcd9b88f4
ldiorio <lisa.diorio@gmail.com>
2023-11-29 22:27:43 -0500

    Update hello_app.js

--- hello_app.js
+++ hello_app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var port = process.env.PORT || 3000;
-//var port = 8080;
+//var port = 8080;   //uncomment to run local
 console.log("This goes to the console window");
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
