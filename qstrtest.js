de87a2667047121f81bffe409875e3666c5e541f
ldiorio <lisa.diorio@gmail.com>
2024-04-16 09:52:53 -0400

    Update qstrtest.js

--- qstrtest.js
+++ qstrtest.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/html'});
    urlObj = url.parse(req.url,true)
-   id = urlObj.id
+   id = urlObj.query.id
    res.write("<h2>This is my query string appication</h2>");
    res.write ("The value of id is: " + id);
    res.end();
