311f0a690a98e6292e90ba039f7d11e39e1e7d98
ldiorio <lisa.diorio@gmail.com>
2024-07-30 14:13:20 -0400

    Update hello_app.js

--- hello_app.js
+++ hello_app.js
@@ -7,9 +7,9 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   urlObj = url.parse(req.url,true)
   id = urlObj.query.id
-   res.write("<h2>This is my hello application</h2>");
-//   res.write ("Success!  This app is deployed online");
+   res.write ("Success!  This app is deployed online");
+  res.write("<h2>This is my hello application</h2>");
   res.write ("The id is: " + id)
-   res.end();
+  res.end();
   console.log('hey')
 }).listen(port);
